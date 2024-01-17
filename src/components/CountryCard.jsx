2e84f7c82668c713be89609dee8cf08576b77bb5
Raidanor <silverstallion30@gmail.com>
2024-01-18 00:47:18 +0400

    added load-in animation for country card

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -11,16 +11,25 @@ const CountryCard = ({ c }) =>
     return(
         <motion.div 
             animate = {{ rotate: rotate ? 360 : 0}}
-            initial = {{}}
-
-            transition={{ duration: 0.25 , type:"tween"}}
+            initial = "hidden"
+            viewport = {{ once:true }}
+            whileInView="visible"
+            transition={{ duration: 0.4, type: "spring", bounce: 0.5 }}
 
             onClick = { () => {setRotate(!rotate) } }
+
+            variants = 
+            {
+                {
+                    visible: { opacity: 1, scale: 1},
+                    hidden: { opacity: 0, scale: 0}
+                }
+            }
             >
             <div className="container border rounded my-3 py-1 country-card-bg" style={{color:'lightblue'}}>
                 
                 <div className="row">
-                    <div className="col">
+                    <div className="col-8">
                         <h2>{c.name}</h2>
                     </div>
                     {/* <div className="col">
